6374f0f662c66400a6d8ae6372953776152d1737
Blob2142 <ulisesfarfan296@gmail.com>
2022-10-14 10:01:37 -0300

    commit

--- prueba-tecnica/front/src/Check.jsx
+++ prueba-tecnica/front/src/Check.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import './App.css'
@@ -6,20 +6,21 @@ import './App.css'
 function Check() {
     const [redirenting, setRedirenting] = useState(true)
     const { key } = useParams()
+    // using the link key look up the link in the database
     axios.get(`/?webKey=${key}`)
         .then(res => res.data)
         .then(res => location.href = res[0].link.includes("https://") ? res[0].link : `https://${res[0].link}`)
         .catch(() => setRedirenting(false))
-
+    // depending on the response it redirects or sends an error
     if (redirenting) {
         return (
-            <div className="Check">
+            <div className="App">
                 <h1>redirecting...</h1>
             </div>
         )
     } else {
         return (
-            <div className="Check">
+            <div className="App">
                 <h1>Error, this link is invalid</h1>
             </div>
         )
