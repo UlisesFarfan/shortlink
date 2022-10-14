b958888c41b65cc7f04b4dc01f27af57b6b3ec5c
Ulises Farfan <94791119+UlisesFarfan@users.noreply.github.com>
2022-10-14 01:01:25 -0300

    Update App.jsx

--- prueba-tecnica/front/src/App.jsx
+++ prueba-tecnica/front/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import copy from "copy-to-clipboard";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import './App.css'
+import './App.css';
 
 function App() {
   const [links, setLinks] = useState([])
